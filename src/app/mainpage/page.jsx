2bfc34c5724c9a91d703411f0250a84c2ab712a2
KepaMax <maljerov.1999@gmail.com>
2024-05-05 21:29:02 +0400

    trending movies added

--- src/app/mainpage/page.jsx
+++ src/app/mainpage/page.jsx
@@ -1,15 +1,35 @@
 import tmdb from '@/components/TMDB';
 import TrendingMovies from '@/components/TrendingMovies';
 
-const page = async () => {
-    const movies = await tmdb.getTrending('movie', 'week');
+const page = () => {
+
+    const fetchMovies = async (text) => {
+        const data = await tmdb.search({
+            type: 'movie',
+            query: "avengers"
+        })
+        console.log(data)
+        return data;
+    }
+    const handleChange = async (e) => {
+        const text = e.target.value;
+        const result = await fetchMovies(text, 1);
+        console.log(result)
+    }
 
     return (
-        <div className='w-max h-max bg-[#1F1D36]'>
+        <div className='w-screen h-screen p-4 bg-[#1F1D36] overflow-hidden'>
             <TrendingMovies />
-            {movies.results.map((movie, index) => (
-                <p key={index}>{movie.title}</p>
-            ))}
+            <div className="relative">
+                <input
+                    type="text"
+                    className="bg-gray-800 text-white focus:outline-none focus:shadow-outline border border-gray-700 rounded-lg py-2 pl-10 pr-4 block w-full appearance-none leading-normal"
+                    placeholder="Search Movies"
+                />
+                <div className="pointer-events-none absolute inset-y-0 left-0 pl-4 flex items-center">
+                    <svg className="fill-current h-4 w-4 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M14.52 13.16l3.6 3.6c.2.2.2.51 0 .71l-1.22 1.22c-.2.2-.51.2-.71 0l-3.6-3.6a8 8 0 1 1 1.92-1.92zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" /></svg>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/TrendingMovies.jsx
+++ src/components/TrendingMovies.jsx
@@ -1,31 +1,27 @@
-import Image from 'next/image';
+import Link from 'next/link';
 import tmdb from './TMDB'
 
 const TrendingMovies = async () => {
-    // const movies = await tmdb.getTrending("movie", "day");
-
-    // return (
-    //     <div className='w-max bg-[#1F1D36]'>{movies.results.map((movie, index) => {
-    //         <div className='rounded-lg flex-col gap-2'>
-    //             <Image
-    //                 priority={true}
-    //                 src={`http://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
-    //                 alt='Logo'
-    //                 width={400}
-    //                 height={100}
-    //             />
-    //             <span>{movie.title}</span>
-    //         </div>
-    //     })}</div>
-    // )
     const movies = await tmdb.getTrending('movie', 'week');
 
     return (
-        <div className='w-max h-max bg-[#1F1D36]'>
-            {movies.results.map((movie, index) => (
-                <p key={index}>{movie.title}</p>
-            ))}
-        </div>
+        <>
+            <h2 className='text-2xl font-medium '>Popular This Week</h2>
+            <div className='w-full flex bg-[#1F1D36] custom-scrollbar gap-5 my-4 overflow-x-auto '>
+                {movies.results.map((movie, index) => (
+                    <Link href={`/movie?id=${movie.id}`} key={index}>
+                        <div className='rounded-lg w-[260px] flex flex-col items-center gap-2'>
+                            <img
+                                className='rounded-lg'
+                                src={`http://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+                                alt='Movie Poster'
+                            />
+                            <p className='text-center text-white'>{movie.title}</p>
+                        </div>
+                    </Link>
+                ))}
+            </div>
+        </>
     )
 }
 
